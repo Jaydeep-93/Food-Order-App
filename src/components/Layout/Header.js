c580a37294c4b3419861334dc9a1a4c617d191ba
Jay <jaydeepkachare123@gmail.com>
2022-03-20 17:46:43 +0530

    button for cart

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton>Cart</HeaderCardButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="mealsImage" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
